78abd11d4886fe4c2cdaa2992dc0c561c77b4a17
IvanLabza <vanalabza77@gmail.com>
2023-11-07 17:25:32 +0200

    Update work-list.js

--- src/js/work-list.js
+++ src/js/work-list.js
@@ -4,17 +4,17 @@ const listElArray = [
   {
     title: 'Personalize',
     text: 'Answer a quick survey about how you express yourself, what causes you stress, and what area would you like to work on. This way, we can fully personalize your journal!',
-    img: 'https://photos.app.goo.gl/pQmFhy1VmKUHV2Tg7',
+    img: 'https://i.postimg.cc/63s9kd1y/img-1.png',
   },
   {
     title: 'Write & Understand',
     text: 'Write, draw, reflect, understand. Alivio will guide you through the prompts and will help you manage your stress!',
-    img: 'https://photos.app.goo.gl/VXEmJK8JfEbZ2EEKA',
+    img: 'https://i.postimg.cc/7L8dDmw8/img-2.png',
   },
   {
     title: 'Alivio!',
     text: 'Now you are aware, and have a way to manage and overcome your own stress.What are you waiting for? Alivio today!',
-    img: 'https://photos.app.goo.gl/yoPPg7DNDkg8PF9P6',
+    img: 'https://i.postimg.cc/X7BsJd2V/img-3.png',
   },
 ];
 
